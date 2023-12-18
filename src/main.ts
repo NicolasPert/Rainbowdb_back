6e02acf166b472c4bf4b87a2fece2f59f3c4d01e
Meranic <meranic77@gmail.com>
2023-12-18 16:00:36 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as passport from 'passport';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api'), app.enableCors();
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  const corsOptions: CorsOptions = {
+    origin: 'https://api.rainbowdb.fr/api/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
   app.use(passport.initialize());
   await app.listen(3000);
 }
